450e80011ec46f90d0a9e6a8643ca350c6baabb9
Tami <catquarks@gmail.com>
2016-08-01 19:11:40 -0400

    passed tests.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,16 @@
-require_relative './class_methods_module.rb'
-require_relative './dance_module.rb'
+# require_relative './class_methods_module.rb'
+# require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+
+	extend FancyDance::ClassMethods #=> you extend for class methods and
+	include FancyDance::InstanceMethods #=> and include for instance methods
+
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+	end
+
+end
